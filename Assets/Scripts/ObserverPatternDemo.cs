3ca8633f385074a8789f8b2fbe0c9ae533f81f04
Henry Roberts <whroberts5@gmail.com>
2021-09-25 16:22:23 -0500

    ui update

---
+++ Assets/Scripts/ObserverPatternDemo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//necessary
+using System;
+
+public class ObserverPatternDemo : MonoBehaviour
+{
+    public event Action PauseGame = delegate { };
+    
+
+
+}
